394efd047b31ca8cbe226793ecf40438ddc9e2b0
Yu Long <johnsonlongyu@gmail.com>
2024-09-22 00:56:24 +0100

    chore: add cd file

--- pkg/mapper/bolt-mapper/mapper.go
+++ pkg/mapper/bolt-mapper/mapper.go
@@ -2,55 +2,11 @@ package bolt_mapper
 
 import (
 	"encoding/json"
-	"time"
 
 	"github.com/boltdb/bolt"
 	"github.com/reimirno/golinks/pkg/mapper"
 	"github.com/reimirno/golinks/pkg/types"
 )
-
-const (
-	BoltMapperConfigType = "BOLT"
-	urlMapBucketName     = "urlMap"
-)
-
-var _ mapper.MapperConfigurer = (*BoltMapperConfig)(nil)
-
-type BoltMapperConfig struct {
-	Name    string `mapstructure:"name"`
-	Path    string `mapstructure:"path"`
-	Timeout int    `mapstructure:"timeout"`
-}
-
-func (b *BoltMapperConfig) GetType() string {
-	return BoltMapperConfigType
-}
-
-func (b *BoltMapperConfig) GetName() string {
-	return b.Name
-}
-
-func (b *BoltMapperConfig) Singleton() bool {
-	return true
-}
-
-func (b *BoltMapperConfig) GetMapper() (mapper.Mapper, error) {
-	var err error
-	var db *bolt.DB
-	db, err = bolt.Open(b.Path, 0600, &bolt.Options{Timeout: time.Duration(b.Timeout) * time.Second})
-	if err != nil {
-		return nil, err
-	}
-	mapper := BoltMapper{
-		name: b.Name,
-		db:   db,
-	}
-	err = mapper.initializeBucket(urlMapBucketName)
-	if err != nil {
-		return nil, err
-	}
-	return &mapper, nil
-}
 
 var _ mapper.Mapper = (*BoltMapper)(nil)
 
